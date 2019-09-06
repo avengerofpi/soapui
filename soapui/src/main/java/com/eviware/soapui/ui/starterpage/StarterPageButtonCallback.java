641a3f52e45b44870d5c0266da2d5e5e69cba508
Cory Washington <tbd>
2019-09-05 23:01:09 -0400

    WIP: working on adding new JavaDocs details

--- soapui/src/main/java/com/eviware/soapui/ui/starterpage/StarterPageButtonCallback.java
+++ soapui/src/main/java/com/eviware/soapui/ui/starterpage/StarterPageButtonCallback.java
@@ -10,16 +10,35 @@ import com.eviware.soapui.model.workspace.Workspace;
 
 import javax.swing.SwingUtilities;
 
+/**
+ * 
+ * This class provides methods for handling particular JavaScript events on the SoapUI Starter Page.
+ *
+ */
 public class StarterPageButtonCallback {
 
+	/**
+	 * Identifier for this event handler class.
+	 */
     public static String CALLBACK = "buttonCallback";
 
     private WorkspaceImpl workspace;
 
+    /**
+     * Creates a new <code>StarterPageButtonCallback</code> instance for the given workspace.
+     * 
+     * @param workspace Object repsenting a SoapUI workspace. This should generally be the currently
+     * loaded workspace being managed by the SoapUI GUI rather than an object you create and manipulate
+     * independently.
+     */
     public StarterPageButtonCallback(Workspace workspace) {
         this.workspace = (WorkspaceImpl) workspace;
     }
 
+    /**
+     * Handle the SoapUI Starter Page 'New SOAP Project' action.
+     * Triggers an <code>Analytics.trackAction</code> event.
+     */
     public void createSoapProject() {
         Analytics.trackAction(SoapUIActions.OS_START_PAGE_NEW_SOAP_PROJECT);
         SwingUtilities.invokeLater(new Runnable() {
@@ -30,6 +49,10 @@ public class StarterPageButtonCallback {
         });
     }
 
+    /**
+     * Handle the SoapUI Starter Page 'New REST Project' action.
+     * Triggers an <code>Analytics.trackAction</code> event.
+     */
     public void createRestProject() {
         Analytics.trackAction(SoapUIActions.OS_START_PAGE_NEW_REST_PROJECT);
         SwingUtilities.invokeLater(new Runnable() {
@@ -40,6 +63,11 @@ public class StarterPageButtonCallback {
         });
     }
 
+    /**
+     * Handle the SoapUI Starter Page 'Test a SOAP API' action.
+     * Triggers an <code>Analytics.trackAction</code> event recording
+     * <link>SoapUIActions.OS_START_PAGE_LAUNCH_EXPLORER</link> label.
+     */
     public void launchEndpointExplorer() {
         Analytics.trackAction(SoapUIActions.OS_START_PAGE_LAUNCH_EXPLORER);
         SwingUtilities.invokeLater(new Runnable() {
@@ -50,6 +78,13 @@ public class StarterPageButtonCallback {
         });
     }
 
+    /**
+     * 
+     * Triggers an <code>Analytics.trackAction</code> event recording <code>location</code>
+     * (unspecified meaning) and <code>SoapUIActions.OS_START_PAGE_TRY_SUI_PRO</code> label.
+     * 
+     * @param location Unknown purpose and expectations for this parameter.
+     */
     public void sendTryProAnalytics(String location) {
         Analytics.trackAction(SoapUIActions.OS_START_PAGE_TRY_SUI_PRO, "Type", location);
     }
