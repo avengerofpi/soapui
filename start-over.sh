125c244997849f1d67081924dddcbb419ae864be
Cory Washington <tbd>
2021-02-22 21:28:04 -0500

    updates to start-over.sh script
    Start adding programatic reference to the script's name (${0}).
    Start work on adding this script to the first commit in the 'start-over'
      branch being created.
    Change `git add -u` to `git add -A`.
    Add '--allow-empty' option to the `git commit` command.

--- start-over.sh
+++ start-over.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+thisScript="${0}";
+
 # Function to `find` and cleanup non-DOS files.
-# Skip .bat files, the .git/ directory, and this script file itself (start-over.sh)
+# Skip (all *.bat files), (the .git/ directory), and (this script file itself (start-over.sh))
 function cleanupNonDosFiles() {
-  find . -path ./.git -prune -o \( -not -iname '*.bat' -a -not -name '*start-over.sh*' \) -a -exec dos2unix {} &> /dev/null \;
-  #find . -path ./.git -prune -o \( -not -iname '*.bat' -a -not -name '*start-over.sh*' \) -a -print -exec dos2unix {} 1> /dev/null \;
-  #find . -type f -not -name '*.bat' -a -not './start-over.sh' -a -not -path './.git/*' -exec dos2unix {} 1> /dev/null \;
+ #find . -path ./.git -prune -o \( -not -iname '*.bat' -a -not -name '*start-over.sh*' \) -a -exec dos2unix {} &> /dev/null \;
+  find . -path ./.git -prune -o \( -not -iname '*.bat' -a -not -name '*start-over.sh*' \)    -exec dos2unix {} &> /dev/null \;
 }
-# -not -iname
 
 # Figure out the branch name to use, if first choice is already taken
 startOverBranchName="";
@@ -20,7 +20,7 @@ for suffix in "" _{01..99}; do
     break;
   fi;
 done;
-
+# Report the branch name that will be used
 if [ -n "${startOverBranchName}" ]; then
   echo "Using 'start over' branch name '${startOverBranchName}'";
 else
@@ -30,14 +30,15 @@ fi;
 # Checkout a new branch from the first commit
 firstCommit="`git log --format=format:%h | tail -1`";
 git checkout -b ${startOverBranchName} ${firstCommit};
+git add "${thisScript}"
 
-# Commit to perform cleanup till
+# Identify the commit we want to perform cleanup until
 targetCommit="next";
 #targetCommit="8a1b8e6b9";
 #targetCommit="a585f5274";
 
-#set -v
-#set -x
+#set -v;
+#set -x;
 
 # Iterate the commits from firstCommit (already checked out) till targetCommit,
 # perform cleanup on each.
@@ -46,14 +47,14 @@ targetCommit="next";
 #   error: pathspec '.' did not match any file(s) known to git
 for c in `git log --format=format:%h%n ${targetCommit} | tac`; do
   echo "commit ${c} - checking out working tree";
-  #git checkout ${c} -- .;
+  echo "   `date`";
   git checkout ${c} .;
   echo "   checked out";
   cleanupNonDosFiles;
   echo "   cleaned up";
-  git add -u;
+  git add -A;
   echo "   updates added";
-  git commit -C ${c} 1> /dev/null;
+  git commit --allow-empty -C ${c} 1> /dev/null;
   echo "   updates committed";
   echo "   cleanup of working tree completed";
 done;
