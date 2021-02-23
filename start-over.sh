5fbdec0cc4df3dfb6c915813e6c5c7648981074a
Cory Washington <tbd>
2021-02-22 21:21:54 -0500

    updates to start-over.sh script
    Fix the command used to find files to run dos2unix on.
    Fix git commit command used.
    Fix loop iteration over commits.
    Add some redirection to get rid of unwanted output.
      Consider piping it to a log file?
    Add a bunch of commented out code; to be used or removed later.
    Update comments.
    Update logging.

--- start-over.sh
+++ start-over.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Function to `find` and cleanup non-DOS files.
+# Skip .bat files, the .git/ directory, and this script file itself (start-over.sh)
 function cleanupNonDosFiles() {
-  find . -type f -not -name '*.bat' -not -path './.git/*' -exec dos2unix {} \;
+  find . -path ./.git -prune -o \( -not -iname '*.bat' -a -not -name '*start-over.sh*' \) -a -exec dos2unix {} &> /dev/null \;
+  #find . -path ./.git -prune -o \( -not -iname '*.bat' -a -not -name '*start-over.sh*' \) -a -print -exec dos2unix {} 1> /dev/null \;
+  #find . -type f -not -name '*.bat' -a -not './start-over.sh' -a -not -path './.git/*' -exec dos2unix {} 1> /dev/null \;
 }
+# -not -iname
 
 # Figure out the branch name to use, if first choice is already taken
 startOverBranchName="";
@@ -29,19 +33,27 @@ git checkout -b ${startOverBranchName} ${firstCommit};
 
 # Commit to perform cleanup till
 targetCommit="next";
+#targetCommit="8a1b8e6b9";
+#targetCommit="a585f5274";
+
+#set -v
+#set -x
 
 # Iterate the commits from firstCommit (already checked out) till targetCommit,
-# perform cleanup on each.  Re-commit the cleaned-up version with the same
-# commit details.
-for c in `git log --format=format:%h ${targetCommit} | tac`; do
+# perform cleanup on each.
+# Re-commit the cleaned-up version with the same commit details.
+# Note that the first commit might not have any files, and thus might give git error msg
+#   error: pathspec '.' did not match any file(s) known to git
+for c in `git log --format=format:%h%n ${targetCommit} | tac`; do
   echo "commit ${c} - checking out working tree";
+  #git checkout ${c} -- .;
   git checkout ${c} .;
-  echo "commit ${c} -   checked out";
-  cleanupNonDosFiles();
-  echo "commit ${c} -   cleaned up";
+  echo "   checked out";
+  cleanupNonDosFiles;
+  echo "   cleaned up";
   git add -u;
-  echo "commit ${c} -   updates added";
-  git commit --amend --no-edit;
-  echo "commit ${c} -   updates committed";
-  echo "commit ${c} - cleanup of working tree completed";
+  echo "   updates added";
+  git commit -C ${c} 1> /dev/null;
+  echo "   updates committed";
+  echo "   cleanup of working tree completed";
 done;
