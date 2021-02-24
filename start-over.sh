05d4030393b5cc442130f2be3e0ea1caeba46e27
Cory Washington <tbd>
2021-02-24 13:43:07 -0500

    updates to start-over.sh script
    Update how the commit sequence is referenced
      firstCommit  -> startingCommit
      targetCommit -> endingCommit
    endingCommit is identified first, and then startingCommit is identified
      as the oldest commit in the endingCommit's log history
    
    Caveat:
      Actually, the oldest commit is skipped, since right now that commit is
      an empty commit. If this script is modified to target an endingCommit
      with a history that includes an initial commit that NOT empty, this
      behavior is probably not what we want.

--- start-over.sh
+++ start-over.sh
@@ -46,25 +46,27 @@ else
   exit;
 fi;
 
-# Checkout a new branch from the first commit
-firstCommit="`git log --format=format:%h | tail -1`";
-git checkout -b ${startOverBranchName} ${firstCommit} || exit 1;
+# Identify the commit we want to perform cleanup until
+endingCommit="next";
+#endingCommit="8a1b8e6b9";
+#endingCommit="a585f5274";
+
+# Identify the commit to start mimicking.
+startingCommit="`git log --format=format:%h ${endingCommit} | tail -1`";
+git checkout -b ${startOverBranchName} ${startingCommit} || exit 1;
 thisScriptCopy="${thisScript}_${suffix}";
 cp "${thisScript}" "${thisScriptCopy}";
 git add "${thisScriptCopy}";
 git commit -m 'add a copy of this script being used to "start over"';
 
-# Identify the commit we want to perform cleanup until
-targetCommit="next";
-#targetCommit="8a1b8e6b9";
-#targetCommit="a585f5274";
-
-# Iterate the commits from firstCommit (already checked out) till targetCommit,
-# perform cleanup on each.
-# Re-commit the cleaned-up version with the same commit details.
-# Note that the first commit might not have any files, and thus might give git error msg
+# Iterate the commits from startingCommit (already checked out) till
+# endingCommit, perform cleanup on each. Create a new commit of the cleaned-up
+# version with the same commit details.
+# Since the 1st commit (8a1b8e6b9 'Initial import.') in this SoapUI repo is
+# empty (no files), it is okay to skip the 1st commit.
+# i.e., git checkout gives git error with msg:
 #   error: pathspec '.' did not match any file(s) known to git
-for c in `git log --format=format:%h%n ${targetCommit} | tac`; do
+for c in `git log --format=format:%h%n ${startingCommit}..${endingCommit} | tac`; do
   commitLineEcho "${c} - checking out this commit's index";
   dateEcho "   `date`";
   echo -n "   git checkout - ";
